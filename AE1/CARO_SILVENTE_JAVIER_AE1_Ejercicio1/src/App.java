0022de42371e19afc862d2b985be09819c16510d
JavierCaroSilvente <jacasi01@floridauniversitaria.es>
2021-09-19 18:56:13 +0200

    AE1-1

---
+++ AE1/CARO_SILVENTE_JAVIER_AE1_Ejercicio1/src/App.java
@@ -0,0 +1,12 @@
+//1. Crea una aplicación de consola en la se utilice una clase que tenga un método “sayHello”
+//que saque por consola “Hola Mundo”. La clase puede tener el nombre que consideres. Como
+//sugerencia, la clase se puede llamar “App”.
+
+public class App {
+	
+	public void SayHello() {
+		System.out.println("Hola Mundo");
+	}
+
+	
+}
